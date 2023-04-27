ddc5685a14a9f0204ca75696cf1c710738e38b94
Uo284548 <Usuario@DESKTOP-5UBJQ90.mshome.net>
2023-04-27 12:28:14 +0200

    SDI-2223-302-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi2223-302-labsoap-sw-server/src/test/java/com/univoi/sdi/Sdi2223302LabsoapSwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univoi.sdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223302LabsoapSwServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
