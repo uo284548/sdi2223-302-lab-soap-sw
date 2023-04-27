0c4c7232230eb129f293982871ad99768a909876
Uo284548 <Usuario@DESKTOP-5UBJQ90.mshome.net>
2023-04-27 12:58:20 +0200

    SDI-2223-302-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi2223-302-lab-soap-sw-client/src/test/java/com/unviovi/sdi/Sdi2223302LabSoapSwClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unviovi.sdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223302LabSoapSwClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
